9712115032ecc6074030f9ced5bf5ed8fe3eddba
woong <hw1000@hanyang.ac.kr>
2024-08-29 12:29:44 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240829/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+
+    private static int[] numbers;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String[] strNumbers = sc.nextLine().split(" ");
+
+        numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(strNumbers[i]);
+        }
+
+        System.out.print(getLcm(n));
+    }
+
+    private static int getLcm(int count) {
+
+        int lcm = numbers[0];
+        for (int i = 0; i < count - 1; i++) {
+            lcm = lcm * numbers[i + 1] / getGcd(lcm, numbers[i + 1]);
+        }
+
+        return lcm;
+    }
+
+    private static int getGcd(int a, int b) {
+
+        if (a % b == 0) {
+            return b;
+        }
+
+        return getGcd(b, a % b);
+    }
+}
