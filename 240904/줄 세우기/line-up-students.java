fb27f602dc75b314bc241c27fa0198fe3966affe
woong <hw1000@hanyang.ac.kr>
2024-09-04 23:01:08 +0900

    Title: 줄 세우기, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240904/줄 세우기/line-up-students.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Student[] students = new Student[n];
+        for (int i = 0; i < n; i++) {
+            students[i] = new Student(sc.nextInt(), sc.nextInt());
+        }
+
+        Arrays.sort(students);
+
+        for (Student s : students) {
+            System.out.println(s);
+        }
+    }
+}
+
+class Student implements Comparable<Student> {
+
+    private Integer h;
+    private Integer w;
+    private Integer number;
+    private static Integer sequence = 0;
+
+    public Student(Integer h, Integer w) {
+        this.h = h;
+        this.w = w;
+        this.number = ++sequence;
+    }
+
+    public Integer getH() {
+        return h;
+    }
+
+    public Integer getW() {
+        return w;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    @Override
+    public int compareTo(Student s) {
+        if (w.equals(s.getW()) && h.equals(s.getH())) {
+            return number.compareTo(s.getNumber());
+            // return s.getNumber().compareTo(number);
+        } else if (h.equals(s.getH())) {
+            return w.compareTo(s.getW());
+            // return s.getW().compareTo(w);
+        } else {
+            // return h.compareTo(s.getH());
+            return s.getH().compareTo(h);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return h + " " + w + " " + number;
+    }
+}
