249efda4764c42c2dc4e5d2841831ac02a9998f4
woong <hw1000@hanyang.ac.kr>
2024-09-05 23:00:18 +0900

    Title: DateTime to DateTime, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240905/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(getMinutes(d, h, m) - getMinutes(11, 11, 11));
+    }
+
+    private static int getMinutes(int d, int h, int m) {
+        int minutes = m;
+        minutes += h * 60;
+        minutes += d * 24 * 60;
+
+        return minutes;
+    }
+}
