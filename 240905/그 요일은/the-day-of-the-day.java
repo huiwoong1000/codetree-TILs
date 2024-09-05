40f180db810aecf3b1e6acb8d3c990182b8ee6cb
woong <hw1000@hanyang.ac.kr>
2024-09-05 23:34:50 +0900

    Title: 그 요일은, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240905/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String strDay = sc.next();
+
+        int diffDays = getAfterDays(m2, d2) - getAfterDays(m1, d1);
+
+        switch (strDay) {
+            case "Tue": diffDays += 1; break;
+            case "Wed": diffDays += 2; break;
+            case "Thu": diffDays += 3; break;
+            case "Fri": diffDays += 4; break;
+            case "Sat": diffDays += 5; break;
+            case "Sun": diffDays += 6; break;
+        }
+
+        System.out.println(diffDays / 7);
+    }
+
+    private static int getAfterDays(int m, int d) {
+
+        int[] days = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int afterDays = d;
+        for (int i = 0; i < m; i++) {
+            afterDays += days[i];
+        }
+
+        return afterDays;
+    }
+}
