fb823674c83f2891a500353d24abb7587ce34a19
woong <hw1000@hanyang.ac.kr>
2024-09-06 11:34:07 +0900

    Title: 10진수로 변환하기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240906/10진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        int result = 0;
+        for (int i = 0; i < str.length(); i++) {
+            result = result * 2 + Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+
+        System.out.print(result);
+    }
+}
