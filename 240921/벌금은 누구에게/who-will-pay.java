20c78a9d740a23d4fa63480a198b7abf8977c2dc
woong <hw1000@hanyang.ac.kr>
2024-09-21 15:55:57 +0900

    Title: 벌금은 누구에게, Time: 300ms, Memory: 19MB, Status: Passed - CodeTree

--- 240921/벌금은 누구에게/who-will-pay.java
+++ 240921/벌금은 누구에게/who-will-pay.java
@@ -9,12 +9,13 @@ public class Main {
         int k = sc.nextInt();
         int[] students = new int[n + 1];
         int ans = -1;
-        
+
         for (int i = 0; i < m; i++) {
             int st = sc.nextInt();
             students[st]++;
             if (students[st] >= k) {
                 ans = st;
+                break;
             }
         }
 
