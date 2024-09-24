bb248da06a6898a974a667856d7ae09f2d73da85
woong <hw1000@hanyang.ac.kr>
2024-09-24 18:55:54 +0900

    Title: 만나는 그 순간, Time: 392ms, Memory: 37MB, Status: Wrong Answer - CodeTree

--- 240924/만나는 그 순간/the-moment-we-meet.java
+++ 240924/만나는 그 순간/the-moment-we-meet.java
@@ -2,15 +2,13 @@ import java.util.*;
 
 public class Main {
 
-    private static final int MAX_LENGTH = 1000 * 1000 + 1;
-
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int[] arrN = new int[MAX_LENGTH];
-        int[] arrM = new int[MAX_LENGTH];
+        List<Integer> NList = new ArrayList<>();
+        List<Integer> MList = new ArrayList<>();
         int position = 0;
         int index = 0;
 
@@ -20,32 +18,34 @@ public class Main {
 
             for (int j = 0; j < t; j++) {
                 if (d == 'R') {
-                    arrN[index++] = ++position;
+                    NList.add(++position);
                 } else {
-                    arrN[index++] = --position;
+                    NList.add(--position);
                 }
             }
         }
 
         position = 0;
         index = 0;
+
         for (int i = 0; i < m; i++) {
             char d = sc.next().charAt(0);
             int t = sc.nextInt();
 
             for (int j = 0; j < t; j++) {
                 if (d == 'R') {
-                    arrM[index++] = ++position;
+                    MList.add(++position);
                 } else {
-                    arrM[index++] = --position;
+                    MList.add(--position);
                 }
             }
         }
 
         int result = -1;
-        for (int i = 0; i < MAX_LENGTH; i++) {
+        int length = (NList.size() > MList.size() ? NList.size() : MList.size());
+        for (int i = 0; i < length; i++) {
             
-            if (arrN[i] == arrM[i]) {
+            if (NList.get(i) == MList.get(i)) {
                 result = i + 1;
                 break;
             }
