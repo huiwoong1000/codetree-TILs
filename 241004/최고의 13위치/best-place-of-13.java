97bbef3c0e3a30e9ad6082d565536bf4fcc0ecf4
woong <hw1000@hanyang.ac.kr>
2024-10-04 13:29:32 +0900

    Title: 최고의 13위치, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241004/최고의 13위치/best-place-of-13.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] coins = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                coins[i][j] = sc.nextInt();
+            }
+        }
+
+        int maxCoinCount = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                maxCoinCount = Math.max(maxCoinCount, coins[i][j] + coins[i][j + 1] + coins[i][j + 1]);
+            }
+        }
+
+        System.out.print(maxCoinCount);
+    }
+}
