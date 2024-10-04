2c91aa31a5ab02cd4e59a37400740083524b829d
woong <hw1000@hanyang.ac.kr>
2024-10-04 18:13:17 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241004/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < n - 3; i++) {
+            for (int j = i + 2; j < n; j++) {
+                maxValue = Math.max(maxValue, numbers[i] + numbers[j]);
+            }
+        }
+
+        System.out.print(maxValue);
+    }
+}
