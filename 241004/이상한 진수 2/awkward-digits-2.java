bf8a4570456e0fa7c53b8911c343018f6eb5b8a2
woong <hw1000@hanyang.ac.kr>
2024-10-04 14:54:06 +0900

    Title: 이상한 진수 2, Time: 111ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241004/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char[] charDigits = sc.next().toCharArray();
+        List<Integer> digits = new ArrayList<>();
+
+        for (int i = 0; i < charDigits.length; i++) {
+            digits.add(charDigits[i] - '0');
+        }
+
+        int maxNum = Integer.MIN_VALUE;
+        for (int i = 1; i < digits.size(); i++) {
+
+            int candidateNumber = 0;
+            for (int j = 0; j < digits.size(); j++) {
+                if (i == j) {
+                    candidateNumber = candidateNumber * 2 + (digits.get(i) == 1 ? 0 : 1);
+                } else {
+                    candidateNumber = candidateNumber * 2 + digits.get(j);
+                }
+            }
+            maxNum = Math.max(maxNum, candidateNumber);
+        }
+
+        System.out.print(maxNum);
+    }
+}
