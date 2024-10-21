d7ed0ba2e78f0ff19234afbac03ae51b78465227
woong <hw1000@hanyang.ac.kr>
2024-10-21 13:43:35 +0900

    Title: 최고의 13위치 2, Time: 181ms, Memory: 10MB, Status: Passed - CodeTree

--- 241021/최고의 13위치 2/best-place-of-13-2.java
+++ 241021/최고의 13위치 2/best-place-of-13-2.java
@@ -13,26 +13,20 @@ public class Main {
             }
         }
 
-        int maxCount = -1;
+        int maxCount = 0;
 
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - 2; j++) {
-                int firstCount = array[i][j] + array[i][j + 1] + array[i][j + 2];
-                int secondCount = 0;
+                for (int k = 0; k < n; k++) {
+                    for (int l = 0; l < n - 2; l++) {
 
-                for (int k = i; k < n - 1; k++) {
-                    for (int l = j; l < n - 2; l++) {
-                        secondCount = array[k + 1][l] + array[k + 1][l + 1] + array[k + 1][l + 2];
-                        if (maxCount < firstCount + secondCount) {
-                            maxCount = firstCount + secondCount;
+                        if (i == k && Math.abs(j - l) <= 2) {
+                            continue;
                         }
 
-                        if (n - l > 5) {
-                            secondCount = array[k][l + 3] + array[k][l + 4] + array[k][l + 5];
-                            if (maxCount < firstCount + secondCount) {
-                                maxCount = firstCount + secondCount;
-                            }
-                        }
+                        int cnt1 = array[i][j] + array[i][j + 1] + array[i][j + 2];
+                        int cnt2 = array[k][l] + array[k][l + 1] + array[k][l + 2];
+                        maxCount = Math.max(maxCount, cnt1 + cnt2);
                     }
                 }
             }
