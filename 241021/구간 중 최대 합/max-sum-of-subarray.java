b7a50a0f6e1adad47c042048e331d1a1aab618ee
woong <hw1000@hanyang.ac.kr>
2024-10-21 15:22:17 +0900

    Title: 구간 중 최대 합, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241021/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        for (int i = 0; i <= n - k; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += numbers[j];
+                max = Math.max(max, sum);
+            }
+        }
+
+        System.out.print(max);
+    }
+}
