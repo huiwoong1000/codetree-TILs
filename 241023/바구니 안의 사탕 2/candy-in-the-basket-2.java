e1a967c8b1daa78eabb2e8c01e555c0a6af83c95
woong <hw1000@hanyang.ac.kr>
2024-10-23 14:36:52 +0900

    Title: 바구니 안의 사탕 2, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 241023/바구니 안의 사탕 2/candy-in-the-basket-2.java
+++ 241023/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -7,11 +7,18 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
         int[] numbers = new int[101];
+        int sumCount = 0;
 
         for (int i = 0; i < n; i++) {
             int candy = sc.nextInt();
             int index = sc.nextInt();
             numbers[index] += candy;
+            sumCount += candy;
+        }
+
+        if (k >= numbers.length) {
+            System.out.print(sumCount);
+            return;
         }
 
         int maxCount = 0;
