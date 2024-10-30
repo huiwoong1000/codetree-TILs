3de4090e596c2ebbcacd6dfc5897224c358fe50a
woong <hw1000@hanyang.ac.kr>
2024-10-30 18:08:39 +0900

    Title: 독서실의 거리두기 5, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241030/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[] seats = sc.next().toCharArray();
+
+        int maxMinDist = 0;
+        for (int i = 1; i < n; i++) {
+            if (seats[i] == '1') {
+                continue;
+            }
+            seats[i] = '1';
+
+            int minDist = Integer.MAX_VALUE;
+            for (int j = 0; j < n - 1; j++) {
+                if (seats[j] == '1') {
+                    for (int k = j + 1; k < n; k++) {
+                        if (seats[k] == '1') {
+                            int dist = k - j;
+                            minDist = Math.min(minDist, dist);
+                        }
+                    }
+                }
+            }
+
+            maxMinDist = Math.max(maxMinDist, minDist);
+            seats[i] = '0';
+        }
+
+        System.out.print(maxMinDist);
+    }
+}
