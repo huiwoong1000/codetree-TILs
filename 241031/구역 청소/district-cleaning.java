6cdd3479d81ce2865a16efb4ab0b4a9e9a1d680f
woong <hw1000@hanyang.ac.kr>
2024-10-31 16:20:07 +0900

    Title: 구역 청소, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 241031/구역 청소/district-cleaning.java
+++ 241031/구역 청소/district-cleaning.java
@@ -9,21 +9,6 @@ public class Main {
         int c = sc.nextInt();
         int d = sc.nextInt();
 
-        int[] area = new int[101];
-        for (int i = a; i < b; i++) {
-            area[i] = 1;
-        }
-        for (int j = c; j < d; j++) {
-            area[j] = 1;
-        }
-
-        int sum = 0;
-        for (int i = 0; i < 101; i++) {
-            if (area[i] == 1) {
-                sum++;
-            }
-        }
-
-        System.out.print(sum);
+        System.out.print((b < c || d < a) ? b - a + d - c : Math.max(b, d) - Math.min(a, c));
     }
 }
