af7fb2d4bf50dad498e5a1c4fac4c160edc4a6b8
woong <hw1000@hanyang.ac.kr>
2024-11-01 13:17:09 +0900

    Title: 순 간 이 동, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 241101/순 간 이 동/teleportation.java
+++ 241101/순 간 이 동/teleportation.java
@@ -9,14 +9,11 @@ public class Main {
         int x = sc.nextInt();
         int y = sc.nextInt();
 
-        int[] candidate = new int[5];
-        candidate[0] = Math.abs(b - a);
-        candidate[1] = Math.abs(x - a) + Math.abs(b - x);
-        candidate[2] = Math.abs(y - a) + Math.abs(b - y);
-        candidate[3] = Math.abs(x - a) + Math.abs(b - y);
-        candidate[4] = Math.abs(y - a) + Math.abs(b - x);
+        int minDist = Integer.MAX_VALUE;
+        minDist = Math.min(minDist, Math.abs(b - a));
+        minDist = Math.min(minDist, Math.abs(x - a) + Math.abs(b - y));
+        minDist = Math.min(minDist, Math.abs(y - a) + Math.abs(b - x));
 
-        Arrays.sort(candidate);
-        System.out.print(candidate[0]);
+        System.out.print(minDist);
     }
 }
