3ab6a8ad4d19e21ba0d66d2c11e99afa523c7807
woong <hw1000@hanyang.ac.kr>
2024-11-05 12:46:42 +0900

    Title: 선택 정렬 구현, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241105/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (numbers[j] < numbers[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = numbers[minIndex];
+            numbers[minIndex] = numbers[i];
+            numbers[i] = temp;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
